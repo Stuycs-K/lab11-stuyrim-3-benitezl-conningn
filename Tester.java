1820e335e69d4c1212aa30c4d32a880e75c420f5
Leonardo Benitez <lbenitez60@stuy.edu>
2025-01-14 08:42:28 -0500

    box side

--- Tester.java
+++ Tester.java
@@ -5,6 +5,12 @@ public class Tester{
       System.out.print("\u2500");
     }
     System.out.println("\u2510");
-
+    for(int i = 0; i < 30; i ++){
+      System.out.print("\u2502");
+      for(int j = 0; j < 40; j++){
+        System.out.print(" ");
+      }
+      System.out.println("\u2502");
+    }
   }
 }
