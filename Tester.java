9125b9b8e51ec07e4e654c4e7b1ff2189590efe5
Leonardo Benitez <lbenitez60@stuy.edu>
2025-01-14 09:37:09 -0500

    full box

--- Tester.java
+++ Tester.java
@@ -1,16 +1,21 @@
 public class Tester{
   public static void main(String[] args) {
-    System.out.print("\u250c");
-    for(int i =0; i < 40; i++){
-      System.out.print("\u2500");
+    System.out.print("┌");
+    for(int i =0; i < 78; i++){
+      System.out.print("─");
     }
-    System.out.println("\u2510");
-    for(int i = 0; i < 30; i ++){
-      System.out.print("\u2502");
-      for(int j = 0; j < 40; j++){
+    System.out.println("┐");
+    for(int i = 0; i < 28; i ++){
+      System.out.print("│");
+      for(int j = 0; j < 78; j++){
         System.out.print(" ");
       }
-      System.out.println("\u2502");
+      System.out.println("│");
     }
+    System.out.print("└");
+    for(int i =0; i < 78; i++){
+      System.out.print("─");
+    }
+    System.out.println("┘");
   }
 }
